4cd06f09f75d23856a21f0bc3f2e180a6cb7ba4a
satorina-vDev <nonato.miau@hotmail.com>
2022-02-04 03:14:41 -0300

    trab5

---
+++ scripy.js
@@ -0,0 +1,28 @@
+//#1
+var jogos = ['mario','sonic','jack and daster','zelda','residentEvil'];
+
+console.log(jogos.length);
+
+//#2
+var jogos = ['mario','sonic','jack and daster','zelda','residentEvil'];
+
+var adicionar = jogos.unshift('strt finght')
+
+console.log(jogos.length);
+
+//#3
+
+var ultimo = jogos.pop();
+
+console.log(jogos.length);
+
+//#4
+var jogoss = ['mario','sonic','jack and daster','zelda','residentEvil'];
+
+var adicionarr = jogoss.push('pac man','minecraft')
+
+console.log(jogos.length);
+//#5
+numbers = [7,5,6,3,8,9,2,1,4]; 
+numbers.sort(); 
+console.log(numbers);
